eb5e3bad1fc721a5188db75c08eff803aa4e432d
mjrgeorge <mjrgeorge@gmail.com>
2021-10-19 21:38:18 +0600

    crud completed

--- src/App.js
+++ src/App.js
@@ -3,17 +3,32 @@ import React, { useState } from "react";
 import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import Paper from '@mui/material/Paper';
-import {ADD_POST} from './GraphQL/Mutations';
-import {DELETE_POST} from './GraphQL/Mutations';
-import {GET_ALL_POSTS} from './GraphQL/Queries';
+import { ADD_POST } from './GraphQL/Mutations';
+import { DELETE_POST } from './GraphQL/Mutations';
+import { UPDATED_POST } from './GraphQL/Mutations';
+import { GET_ALL_POSTS } from './GraphQL/Queries';
 import { Avatar, Button, Container, IconButton, List, ListItem, ListItemIcon, ListItemSecondaryAction, ListItemText, TextField, Typography } from "@mui/material";
 const App = () => {
+  const [editBtnView, setEditBtnView] = useState(false);
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  const [editedId, setEditedId] = useState("");
 
   const { loading, error, data } = useQuery(GET_ALL_POSTS);
 
   const [createNewPost] = useMutation(ADD_POST, {
+    onCompleted(data) {
+      setTitle('');
+      setContent('');
+    },
+    refetchQueries: [
+      {
+        query: GET_ALL_POSTS,
+      }
+    ]
+  });
+
+  const [updatePost] = useMutation(UPDATED_POST, {
     onCompleted(data) {
       setTitle('');
       setContent('');
@@ -36,58 +51,79 @@ const App = () => {
     ]
   });
 
+  const handleCreatePost = () => {
+    createNewPost({ variables: { title: title, content: content } });
+  };
+
+  const handleEditButton = (editId, editTitle, editContent) => {
+    setEditBtnView(true);
+    setEditedId(editId);
+    setTitle(editTitle);
+    setContent(editContent);
+  };
+
+  const handleEditPost = () => {
+    updatePost({ variables: { id: editedId, title: title, content: content } });
+  };
+
+  const handleDeleteButton = (id) => {
+    deletePost({ variables: { id: id } });
+  };
+
   if (loading) return <h1>Loading...</h1>;
   if (error) return <h1>Error...</h1>;
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    createNewPost({ variables: { title: title, content: content } });
-    console.log(title, content);
-  }
-  const deleteSinglePost = (id) => {
-    deletePost({ variables: { id: id } });
-    console.log(id);
-  }
 
   return (
     <Container maxWidth="sm">
       <Typography color="error" align="center" variant="h4" gutterBottom>
         GraphQL with React Js
       </Typography>
-      <form onSubmit={handleSubmit}>
-        <TextField
-          value={title}
-          label='Enter Your Title'
-          variant="outlined"
-          onChange={(e) => setTitle(e.target.value)}
-          fullWidth
-        />
-        <br />
-        <br />
-        <TextField
-          value={content}
-          label='Enter Your Content'
-          variant="outlined"
-          onChange={(e) => setContent(e.target.value)}
-          fullWidth
-        />
-        <br />
-        <br />
+      <TextField
+        value={title}
+        label='Enter Your Title'
+        variant="outlined"
+        onChange={(e) => setTitle(e.target.value)}
+        fullWidth
+      />
+      <br />
+      <br />
+      <TextField
+        value={content}
+        label='Enter Your Content'
+        variant="outlined"
+        onChange={(e) => setContent(e.target.value)}
+        fullWidth
+      />
+      <br />
+      <br />
+      {!editBtnView ?
         <Button
-          type="submit"
+          variant="contained"
+          color="primary"
+          size="large"
+          fullWidth
+          disabled={!title || !content}
+          onClick={handleCreatePost}
+        >
+          Add
+        </Button>
+        :
+        <Button
           variant="contained"
           color="secondary"
           size="large"
           fullWidth
-          disabled={!title || !content}
+          onClick={handleEditPost}
         >
-          Submit
-        </Button>
-      </form>
+          Edit
+        </Button>}
       <br />
       <br />
       <Paper elevation={3} >
         <List>
+          {/* {
+            (data.allPost.length) === 0(<Typography variant="h6" color="error">Post Empty!</Typography>):''
+          } */}
           {
             data?.allPost?.map((item, i) => (
               <ListItem button key={i}>
@@ -100,10 +136,10 @@ const App = () => {
                 </ListItemIcon>
                 <ListItemText primary={item?.title} secondary={item?.content} />
                 <ListItemSecondaryAction>
-                  <IconButton>
+                  <IconButton onClick={() => handleEditButton(item?.id, item?.title, item?.content)}>
                     <EditIcon color="primary" />
                   </IconButton>
-                  <IconButton onClick={() => deleteSinglePost(item?.id)}>
+                  <IconButton onClick={() => handleDeleteButton(item?.id)}>
                     <DeleteIcon color="secondary" />
                   </IconButton>
                 </ListItemSecondaryAction>
--- src/GraphQL/Mutations.js
+++ src/GraphQL/Mutations.js
@@ -1,9 +1,17 @@
 import { gql } from "@apollo/client";
 
 export const ADD_POST = gql`
-mutation CreateNewPost ($title: String!, $content: String!) {
+mutation CreateNewPost ($title: String! $content: String!) {
   createNewPost(title: $title, content: $content) {
     createdStatus
+  }
+}
+`;
+
+export const UPDATED_POST = gql`
+mutation UpdatePost($id: Int! $title: String! $content: String!){
+  updatePost(postId: $id, title: $title, content: $content) {
+    updatedStatus
   }
 }
 `;
